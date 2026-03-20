f0584908c402d4909ecec7f641b376db22cfd457
isabmoraiss <isa.morais121108@gmail.com>
2026-03-20 16:56:33 -0300

    Create Pessoa.cs

---
+++ Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Pessoa
+{
+    class Pessoa
+
+    {
+        public string nome;
+        public string email;
+        public string login;
+        public string senha;
+
+        public void provarexixtencia()
+        {
+            Console.WriteLine("oi, eu existo!");
+        }
+       
+    }
+}
